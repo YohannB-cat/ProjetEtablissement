a39e1b4c3118a202ce6abc935fe7c7826eba6cc1
YohannB-cat <bachelier.yohann@gmail.com>
2020-03-13 14:54:10 +0100

    initial project stucture

---
+++ src/main/java/com/fr/adaming/ProjetEtablissementApplication.java
@@ -0,0 +1,13 @@
+package com.fr.adaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEtablissementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetEtablissementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fr/adaming/ProjetEtablissementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fr.adaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEtablissementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
