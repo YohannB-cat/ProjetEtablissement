4d583b8a2a8d2bcbe5e4b4601b47f7660674240a
FlavienGomila <flavien.gomila@gmail.com>
2020-03-16 09:34:47 +0100

    Creation de AbsanceConverter

--- src/main/java/com/fr/adaming/controller/AbsanceController.java
+++ src/main/java/com/fr/adaming/controller/AbsanceController.java
@@ -2,18 +2,26 @@ package com.fr.adaming.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 
-import com.fr.adaming.dto.EtudiantDto;
-import com.fr.adaming.dto.EtudiantDtoCreate;
+import com.fr.adaming.converter.EtudiantConverter;
+import com.fr.adaming.dto.AbsanceDto;
 import com.fr.adaming.dto.ResponseDto;
+import com.fr.adaming.service.IEtudiantService;
 
 public class AbsanceController implements IAbsanceController {
 
+	@Autowired
+	@Qualifier("absanceservice")
+	private IAbsanceService service;
+	
 	@Override
 	public ResponseEntity<ResponseDto> create(@Valid AbsanceDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		AbsanceDto etu = 
+				AbsanceConverter.convertAbsanceToAbsanceDto()
+				service.create(AbsanceConverter.convertAbsanceDtoToAbsance(dto)));
 	}
 
 	@Override
