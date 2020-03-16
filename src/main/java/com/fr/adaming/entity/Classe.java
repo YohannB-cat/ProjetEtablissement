c574d465e5916b6e04287ffa3100b1555692f95d
FlavienGomila <flavien.gomila@gmail.com>
2020-03-16 17:45:08 +0100

    Les ServicesTests

--- src/main/java/com/fr/adaming/entity/Classe.java
+++ src/main/java/com/fr/adaming/entity/Classe.java
@@ -8,10 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-
-import org.hibernate.annotations.ManyToAny;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/com/fr/adaming/entity/Note.java
+++ src/main/java/com/fr/adaming/entity/Note.java
@@ -1,12 +1,10 @@
 package com.fr.adaming.entity;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
