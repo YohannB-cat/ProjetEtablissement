70917a80a6d1bbd6b9a21bfe5b60a0ef7ed6e8a7
FlavienGomila <flavien.gomila@gmail.com>
2020-03-17 12:17:04 +0100

    Suppression classe 'classeDao' et ajout de méthode dans 'INiveauDao'

--- src/main/java/com/fr/adaming/dao/INiveauDao.java
+++ src/main/java/com/fr/adaming/dao/INiveauDao.java
@@ -1,10 +1,19 @@
 package com.fr.adaming.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.fr.adaming.entity.Classe;
 import com.fr.adaming.entity.Niveau;
 
 @Repository
-public interface INiveauDao extends JpaRepository<Niveau, Integer>{
+public interface INiveauDao extends JpaRepository<Niveau, Integer> {
+	
+	@Query(value = "FROM Classe WHERE id = (SELECT classe_id FROM Niveau WHERE id = :idNiveau)")
+	public List<Classe> listDesClasses(@Param(value = "idNiveau") int idNiveau);
+	
 }
