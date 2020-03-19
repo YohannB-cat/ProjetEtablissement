8243a77ac75573f7ffedc8f1e9fcb55c20589cfa
unknown <maxime.rembert@gmail.com>
2020-03-19 14:26:58 +0100

    annotation @bean en commentaire car pb start api

--- src/main/java/com/fr/adaming/config/SwaggerConfig.java
+++ src/main/java/com/fr/adaming/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-	@Bean
+//	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build();
