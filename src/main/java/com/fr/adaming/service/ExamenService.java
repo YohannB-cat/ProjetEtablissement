6a3fa8b14bce892518fda60a09071f11b2e9a423
YohannB-cat <bachelier.yohann@gmail.com>
2020-03-20 16:14:36 +0100

    oo

--- src/main/java/com/fr/adaming/service/ExamenService.java
+++ src/main/java/com/fr/adaming/service/ExamenService.java
@@ -2,7 +2,6 @@ package com.fr.adaming.service;
 
 import java.util.List;
 
-import javax.validation.ConstraintViolationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
