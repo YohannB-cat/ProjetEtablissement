7c9240c99a6361df8e2319a532a1767ff1cfa6e6
FlavienGomila <flavien.gomila@gmail.com>
2020-03-23 11:57:34 +0100

    JavaDoc des classes Classe et Niveau

--- src/main/java/com/fr/adaming/dao/INiveauDao.java
+++ src/main/java/com/fr/adaming/dao/INiveauDao.java
@@ -10,9 +10,21 @@ import org.springframework.stereotype.Repository;
 import com.fr.adaming.entity.Classe;
 import com.fr.adaming.entity.Niveau;
 
+/**
+ * Interface INiveauDao reponsable de la couche dao de l'entité Niveau. Contient une methodes particulières qui permet de rechercher la liste des classes qui dépendent du niveau en question.
+ * @author Flavien
+ * @since 1.0.x
+ *
+ */
 @Repository
 public interface INiveauDao extends JpaRepository<Niveau, Integer> {
 	
+	
+	/**
+	 * Methode de recherche des Classes du même Niveau, par id de ce Niveau
+	 * @param idNiveau l'Id du niveau dont on veut les classes
+	 * @return Renvoie la liste des Classes trouvé
+	 */
 	@Query(value = "FROM Classe WHERE id_niveau = :idNiveau")
 	public List<Classe> findListClasseByIdNiveau(@Param(value = "idNiveau") int idNiveau);
 	
