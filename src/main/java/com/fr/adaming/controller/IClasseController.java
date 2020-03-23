3ee243d2d2aec0f8df37e923e7af25d462470571
FlavienGomila <flavien.gomila@gmail.com>
2020-03-23 12:20:15 +0100

    Correction de la javaDoc de IClasseController et INiveauController

--- src/main/java/com/fr/adaming/controller/IClasseController.java
+++ src/main/java/com/fr/adaming/controller/IClasseController.java
@@ -20,34 +20,34 @@ public interface IClasseController {
 	/**
 	 * Methode de création d'une nouvelle Classe
 	 * @param dto accepte un ClasseDto sans id qui sera ensuite auto-générer.
-	 * @return Renvoie un ResponseEnttity<ResponseDto> avec un boolean erreur, un String message, et un objet.
+	 * @return Renvoie un ResponseEntity-ResponseDto avec un boolean erreur, un String message, et un objet.
 	 */
 	public ResponseEntity<ResponseDto> create(@Valid @RequestBody ClasseDtoCreate dto);
 
 	/**
 	 * Methode de modification d'une Classe
 	 * @param dto accepte un ClasseDtoCreate avec l'id de l'objet à modifié et les paramètre du nouvel objet.
-	 * @return Renvoie un ResponseEnttity<ResponseDto> avec un boolean erreur, un String message, et un objet.
+	 * @return Renvoie un ResponseEntity-ResponseDto avec un boolean erreur, un String message, et un objet.
 	 */
 	public ResponseEntity<ResponseDto> update(@Valid @RequestBody ClasseDtoCreate dto);
 
 	/**
 	 * Methode de recherche d'une Classe par id
 	 * @param id accepte l'id de l'objet à chercher. Cette id est placé dans la requète Get
-	 * @return Renvoie un ResponseEnttity<ResponseDto> avec un boolean erreur, un String message, et un objet.
+	 * @return Renvoie un ResponseEntity-ResponseDto avec un boolean erreur, un String message, et un objet.
 	 */
 	public ResponseEntity<ResponseDto> findById(@PathVariable(name = "id") int id);
 
 	/**
 	 * Methode de recherche de la liste des Classes, n'accepte aucun param.
-	 * @return Renvoie un ResponseEnttity<ResponseDto> avec un boolean erreur, un String message, et un objet.
+	 * @return Renvoie un ResponseEntity-ResponseDto avec un boolean erreur, un String message, et un objet.
 	 */
 	public ResponseEntity<ResponseDto> findAll();
 
 	/**
 	 * Methode de suppression d'une Classe par id
 	 * @param id accepte l'id de l'objet à supprimer. Cette id est placé dans la requète Get
-	 * @return Renvoie un ResponseEnttity<ResponseDto> avec un boolean erreur, un String message, et un objet.
+	 * @return Renvoie un ResponseEntity-ResponseDto avec un boolean erreur, un String message, et un objet.
 	 */
 	public ResponseEntity<ResponseDto> delete(@PathVariable(name = "id") int id);
 
